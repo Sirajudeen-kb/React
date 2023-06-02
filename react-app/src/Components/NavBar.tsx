a8b2fde5182d5648073077b6b11b33fab5e20a7e
Sirajudeen <kbsiraj12@gmail.com>
2023-06-02 14:37:01 +0530

    second

--- react-app/src/Components/NavBar.tsx
+++ react-app/src/Components/NavBar.tsx
@@ -5,7 +5,14 @@ function NavBar() {
 
 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#">
+            <img
+              src="https://img.icons8.com/external-nawicon-flat-nawicon/64/external-book-back-to-school-nawicon-flat-nawicon-2.png"
+              alt="Book"
+              width="40"
+              height="40"
+            />
+          </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
